118e2c31257675221fad83401dbb74763d79cf73
Bart Sokol <sokol.bartosz@gmail.com>
2015-12-01 19:50:07 +0100

    Zainicjowanie repozytorium materiałami z poprzednich zajęć

---
+++ kod/UamTTA/UamTTA/Storage/UamTTAContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity;
+using UamTTA.Model;
+
+namespace UamTTA.Storage
+{
+    public class UamTTAContext : DbContext
+    {
+        public UamTTAContext() : base("UamTTAContext")
+        {
+        }
+
+        public DbSet<Account> Accounts { get; set; }
+
+        public DbSet<Budget> Budgets { get; set; }
+
+        public DbSet<Transfer> Transfers { get; set; }
+
+        public DbSet<BudgetTemplate> Templates { get; set; }
+    }
+}
